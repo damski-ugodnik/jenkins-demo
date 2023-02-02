0a5ce4f18b6436f68bd8dd0d05aa9af1ac5f4f55
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-02-02 14:16:34 +0200

    initial commit

---
+++ src/main/java/com/griddynamics/democicd/Controller.java
@@ -0,0 +1,13 @@
+package com.griddynamics.democicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    public ResponseEntity<?> getResponse(String param) {
+        return ResponseEntity.ok(param);
+    }
+}
