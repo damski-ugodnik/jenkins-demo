96c4551121141ff846876c1de07f98cd165caa2f
damski-ugodnik <ponomarenkonikita03@gmail.com>
2023-02-02 15:08:13 +0200

    deployment pipeline

--- src/main/java/com/griddynamics/democicd/Controller.java
+++ src/main/java/com/griddynamics/democicd/Controller.java
@@ -1,12 +1,14 @@
 package com.griddynamics.democicd;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+    @GetMapping
     public ResponseEntity<?> getResponse(String param) {
         return ResponseEntity.ok(param);
     }
